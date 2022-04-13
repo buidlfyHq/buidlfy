f89335c991d2f932d98f262b85c42a3271771147
paarth17032000 <paarth17032000@gmail.com>
2022-04-14 03:03:48 +0530

    dashboard initialize

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Webflow-UI
-    </h1>
+    <div className='flex flex-row min-h-screen'>
+      <div className='fixed left-0 top-0 z-0 w-[250px] border-r bg-blue-300 h-full'>sidebar</div>
+      <div className='flex-1'>
+        <div className='fixed left-[250px] h-[60px] w-full top-0 bg-green-500'>navbar</div>
+        <div className='fixed ml-[250px] bg-yellow-500'>main</div>
+      </div>
+    </div>
   )
 }
 
