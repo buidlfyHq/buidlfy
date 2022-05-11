859cf8bc762c73cbe602f4dc1992f73b3fa77e65
paarth17032000 <paarth17032000@gmail.com>
2022-05-11 13:55:16 +0530

    feat/modifiying logoname fixed

--- src/components/dashboard/userNav.tsx
+++ src/components/dashboard/userNav.tsx
@@ -8,7 +8,7 @@ import SettingComponent from '../utils/SettingComponent';
 
 export default function UserNav() {
     const [navClass, setNavClass] = useState<String>('')
-    const [navbar, setnNavbar] = useState<Number>(0)
+    const [navbar, setNavbar] = useState<Number>(0)
     const [brandName, setBrandName] = useState<String>('Spheron')
     // const NavShowHide = () => {  
     //     if(openNav === 0) setOpenNav(1)
@@ -16,19 +16,19 @@ export default function UserNav() {
     // }
 
     const [menuArr, setMenuArr] = useState([
-        'Home'
+        { name : 'Home', link: ''}
     ])
 
     const handleAddMenu = () => {
         setMenuArr([
-          ...menuArr, 'Menu'
+          ...menuArr, { name: 'Menu', link: ''}
         ])
     }
 
     useEffect(() => {
         const navData = {
             "navbar": {
-                "logo": {brandName},
+                "logo": `${brandName}`,
                 "menus": menuArr,
                 "connectWallet": {}
             }
@@ -42,28 +42,28 @@ export default function UserNav() {
             {navbar === 0 ? (
                 <div className='relative'>
                     <div className={`flex justify-evenly items-center h-[60px] border-b p-4 relative`} id="originalnav">
-                        <div className="flex items-center justify-start w-full">
-                            <SettingComponent classname={'mx-2 font-bold'} text={brandName} />
+                        <div className="flex items-center justify-start ">
+                            <SettingComponent classname={'mx-2 font-bold'} text={brandName} link={''} setBrandName={setBrandName} />
                             {/* <div className="mx-2 font-bold">{brandName}</div> */}
                         </div>
                         <div className="flex items-center justify-center w-full">
-                            <div className="flex items-center justify-end">
-                            {menuArr.map((text, index) => {
+                            <div className="flex items-center justify-end ">
+                            {menuArr.map((obj, index) => {
                                 return(
                                     <div id={`${index}`}>
-                                        <SettingComponent classname={'mx-1'} text={text} />
+                                        <SettingComponent classname={'mx-1'} text={obj.name} link={obj.link} setMenuArr={setMenuArr} menuArr={menuArr} id={`${index}`} />
                                     </div>
                                 )
                             })}
                             </div>
                             <GrFormAdd className="ml-2 rounded-[8px] hover:bg-[#f5efef] text-[40px] p-2 cursor-pointer" onClick={handleAddMenu} />
                         </div>
-                        <div className="flex items-center justify-end w-full">
-                            <div className="px-6 py-2 text-white bg-purple-400 cursor-pointer rounded-xl">Connect Wallet</div>
+                        <div className="flex items-center justify-end w-auto">
+                            <div className="px-6 py-2 text-white bg-purple-400 cursor-pointer whitespace-nowrap rounded-xl">Connect Wallet</div>
                         </div>
                         {/* sudo */}
                         <div className={`flex justify-start items-center h-[60px] p-4 w-full absolute`} id="nav">
-                            <div className='flex items-center px-3 py-2 bg-white border shadow-md cursor-pointer hover:text-purple-700' onClick={() => setnNavbar(1)}>
+                            <div className='flex items-center px-3 py-2 bg-white border shadow-md cursor-pointer hover:text-purple-700' onClick={() => setNavbar(1)}>
                                 <div>Delete</div>
                                 <AiOutlineDelete className='text-[18px] ml-2' />
                             </div>
@@ -72,7 +72,7 @@ export default function UserNav() {
                 </div>
             ) : (
                 // flex items-center px-3 py-2 bg-white border shadow-md cursor-pointer hover:text-purple-700'
-                <div className="flex justify-evenly items-center h-[50px] border bg-white hover:text-purple-700 shadow-md m-2 py-2 px-3 w-[150px] cursor-pointer" onClick={() => setnNavbar(0)}>
+                <div className="flex justify-evenly items-center h-[50px] border bg-white hover:text-purple-700 shadow-md m-2 py-2 px-3 w-[150px] cursor-pointer" onClick={() => setNavbar(0)}>
                     <div>Add Navbar</div>
                     <IoMdAddCircleOutline className="text-[18px]" />
                 </div>
--- src/components/utils/SettingComponent.tsx
+++ src/components/utils/SettingComponent.tsx
@@ -4,9 +4,15 @@ import {RiText} from 'react-icons/ri'
 import {AiOutlineLink, AiOutlineDelete} from 'react-icons/ai'
 import {VscSymbolColor} from 'react-icons/vsc'
 
-export default function SettingComponent({classname, text}) {
+export default function SettingComponent({classname, text, link, setBrandName = null ,setMenuArr = null, id=null, menuArr=null}) {
+  // console.log(typeof id, 2321, id)
   const [open, setOpen] = useState<Boolean>(false)
+  const [textVal, setTextVal] = useState<string>(text || '')
+  const [linkVal, setLinkVal] = useState<string>(link || '')
+  const [newMenuArr, setNewMenuArr] = useState<Object []>(menuArr)
+  const Id:string = id
   const ref = useRef(null)
+  console.log(newMenuArr)
 
   useEffect(() => {
     let handler = (event: Event) => {
@@ -14,32 +20,55 @@ export default function SettingComponent({classname, text}) {
         setOpen(false)
       }
     }
-
     document.addEventListener("mousedown", handler)
-
     return () => {
       document.removeEventListener("mousedown", handler)
     }
   })
 
+  useEffect(() => {}, [setTextVal])
+
+  const deleteMenu = (e: any, Id:string) => {
+    console.log(Id)
+
+    // console.log(2)
+    // setOpen(false) 
+  }
+
+  const handleTextChange = (e: any, Id:string) => {
+    setTextVal(e.target.value)
+    if(Id === null){setBrandName(e.target.value)}
+    console.log(newMenuArr)
+    let obj = 
+    console.log(newMenuArr[parseInt(e.target.name)]) 
+
+    // setMenuArr([
+      
+    // ])
+  }
+
+  const handleLinkChange = (e: any, Id:string) => {
+    setLinkVal(e.target.value)
+  }
+ 
+
   return (
     <>
       <div className={`${classname} rounded-[8px] hover:bg-[#f5efef] py-2 px-4 cursor-pointer relative`} onClick={() => setOpen(true)}>
         {text}
-      </div>
-      {open ? (
-        <div className='border menu' ref={ref}>
+        {open ? (
+        <div className='border shadow-sm menu' ref={ref}>
           <div className='px-3 my-1 text-gray-500'>Settings</div>
           <div className='flex items-center px-3 mt-1'>
             <RiText className='text-[18px] mr-3' />
-            <input className="changeText" type='text' placeholder='change text' />
+            <input name={Id} value={textVal} onChange={(e) => handleTextChange(e,Id)} className="changeText" type='text' placeholder='Name..' />
           </div>
           <div className='flex items-center px-3 mt-2'>
             <AiOutlineLink className='text-[18px] mr-3' />
-            <input className="changeText" type='text' placeholder='change text' />
+            <input name={Id} value={linkVal} onChange={(e) => handleLinkChange(e,Id)} className="changeText" type='text' placeholder='URL...' />
           </div>
           <div className='h-[1px] w-full bg-gray-200 mt-3 mb-1'></div>
-          <div className='flex items-center w-full px-3 py-2 text-gray-600 rounded cursor-pointer hover:bg-slate-100'  onClick={() => setOpen(false)}>
+          <div className='flex items-center w-full px-3 py-2 text-gray-600 rounded cursor-pointer hover:bg-slate-100' onClick={(e) => deleteMenu(e,Id)}>
             <AiOutlineDelete className='text-[18px] mr-3' />
             <span>Delete</span>
           </div>
@@ -49,6 +78,7 @@ export default function SettingComponent({classname, text}) {
           </div>
         </div>
       ) : null}
+      </div>
     </>
   )
 }
