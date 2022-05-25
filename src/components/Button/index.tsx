ce5104c6da3c437e063b2c8b61511db80f8166a5
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-05-25 19:34:40 +0530

    feat/style-component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ const Button: FC = () => {
   return (
     <>
       <div className=" flex px-6 py-2 items-center justify-start w-auto">
-        <div className="btn px-6 py-2 shadow-sm	bg-white border border-solid rounded w-48 cursor-pointer whitespace-nowrap">
+        <div className="btn px-6 py-2 rounded w-48 cursor-pointer whitespace-nowrap">
           Add Button
         </div>
       </div>
