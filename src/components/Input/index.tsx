75f505d86a5878a57845c9799a9af2060633815b
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-05-26 15:19:17 +0530

    feat/id-changes

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,7 +9,7 @@ const Input: FC = () => {
   return (
     <>
       <input
-        className="input bg-white appearance-none ml-6 border border-solid rounded py-2 px-3 text-gray-700 leading-tight"
+        className="input bg-white appearance-none ml-6 border border-solid rounded px-3 text-gray-700 leading-tight"
         id="input"
         type="text"
         placeholder="Input"
