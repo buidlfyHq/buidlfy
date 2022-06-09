efb8f48f097f04185b6c1be1eb3510b83e637e8c
Aayush <mahapatraaayush@gmail.com>
2022-06-09 16:48:23 +0530

    feat/created-monorepo

--- src/interfaces/styles.ts
+++
@@ -1,9 +0,0 @@
-export default interface IStyles {
-  color?: object;
-  fontWeight?: string;
-  fontStyle?: string;
-  textDecoration?: string;
-  justifyContent?: string;
-  fontSize?: number;
-  deleteComponent: number;
-}
