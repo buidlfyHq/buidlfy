b2f9d091c9fca02acb13af50fcd93888fecd43ad
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-07-15 17:56:18 +0530

    fix: commented conflicts

--- decoder/src/hooks/on-load.tsx
+++ decoder/src/hooks/on-load.tsx
@@ -1,5 +1,5 @@
 import { ethers, providers, Contract, Signer } from "ethers";
-
+// work in progress
 export const onLoad = (config: any) => {
   let provider: providers.Web3Provider,
     signer: providers.Provider | Signer,
