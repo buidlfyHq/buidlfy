775e207fada15c52df54903d6042f699a07ed4a5
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-07-22 01:21:07 +0530

    Fix UI issues after deployemnt - Connect wallet UI issue changed to switched button and UI change of toggle - Border radius change to 0 - Border width and border radius can go back to 0 from 1

--- builder/src/components/Utils/ConnectSwitchComponent.tsx
+++ builder/src/components/Utils/ConnectSwitchComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import "styles/Dashboard.css";
 import "styles/Components.css";
 
@@ -11,8 +11,10 @@ const ConnectSwitchComponent: FC<IConnectSwitchComponent> = ({
   setOn,
   connectWallet,
 }) => {
-  const [textToggle, textToggleState] = useState<boolean>(true);
-
+  const [textToggle, textToggleState] = useState(
+    connectWallet === "on" ? true : false
+  );
+  console.log(connectWallet, "connect");
   const handleOnChange = () => {
     textToggleState(!textToggle);
     if (connectWallet === "on") {
@@ -21,17 +23,24 @@ const ConnectSwitchComponent: FC<IConnectSwitchComponent> = ({
       setOn(true);
     }
   };
-
+  useEffect(() => {
+    textToggleState(connectWallet === "on" ? true : false);
+  }, [connectWallet]);
   return (
-    <span className="flex items-center text-left px-3 mt-2 mb-0 text-xl text-gray-500 font-regular font-normal not-italic">
-      Connect Wallet
-      <div>
-        <span
-          onClick={handleOnChange}
-          className="flex ml-3 items-center justify-center shadow text-[18px] w-8 h-10 my-2 font-regular"
-        >
-          {textToggle ? "On" : "Off"}
-        </span>
+    <span className="flex items-center">
+      <span className="text-left px-3 mt-2 mb-0 text-l text-gray-500 font-regular font-normal not-italic">
+        Connect Wallet
+      </span>
+      <div className="flex justify-center ml-3 mt-1">
+        <div onClick={handleOnChange} className="form-check form-switch w-14">
+          <input
+            className="form-check-input w-12 -ml-10 bg-blue rounded-full float-left h-5 align-top cursor-pointer shadow-sm"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+            checked={textToggle ? true : false}
+          />
+        </div>
       </div>
     </span>
   );
