0a4a53781eebcf19d046670191f4336d088b29e1
aayushmahapatra <dr.aaaaaaaa8@gmail.com>
2022-08-03 12:52:13 +0530

    Fix decoder issues in decoder
    Grid Layout in container was not getting width.

--- decoder/src/components/custom-components/container.tsx
+++ decoder/src/components/custom-components/container.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import GridLayout from "react-grid-layout";
 import RenderItem from "utils/render-item";
 import IBgContainer from "interfaces/container";
@@ -19,49 +19,55 @@ const Container: FC<IBgContainer> = ({
   outputValue,
   setOutputValue,
 }) => {
-  let containerW = document
-    ?.getElementById(`${item.i}`)
-    ?.getBoundingClientRect().width;
+  const [containerW, setContainerW] = useState(null);
+
+  useEffect(() => {
+    const cw = document
+      ?.getElementById(`${item.i}`)
+      ?.getBoundingClientRect().width;
+    if (cw) setContainerW(cw);
+  }, []);
 
   return (
     <section id={item.i} className="w-full h-fit">
-      <GridLayout
-        layout={children}
-        cols={6}
-        rowHeight={50}
-        width={containerW || 500}
-        isDraggable={false}
-        isResizable={false}
-        compactType={null}
-        margin={[0, 0]}
-        className="h-full"
-        style={{
-          backgroundColor: `rgba(${backgroundColor.r}, ${backgroundColor.g}, ${backgroundColor.b}, ${backgroundColor.a})`,
-          borderColor: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
-          borderRadius: `${borderRadius}px`,
-          borderWidth: `${borderWidth}px`,
-          backgroundImage: `url(${imgData})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          boxShadow: shadow,
-        }}
-      >
-        {children.map((c: IItems) => {
-          const { x, y, w, h, minW, i } = c;
-          return (
-            <div key={i} data-grid={{ x, y, w, h, minW }}>
-              <RenderItem
-                item={c}
-                inputValue={inputValue}
-                setInputValue={setInputValue}
-                outputValue={outputValue}
-                setOutputValue={setOutputValue}
-              />
-            </div>
-          );
-        })}
-      </GridLayout>
+      {containerW && (
+        <GridLayout
+          layout={children}
+          cols={6}
+          rowHeight={50}
+          width={containerW || 1000}
+          isDraggable={false}
+          isResizable={false}
+          compactType={null}
+          margin={[0, 0]}
+          style={{
+            backgroundColor: `rgba(${backgroundColor.r}, ${backgroundColor.g}, ${backgroundColor.b}, ${backgroundColor.a})`,
+            borderColor: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
+            borderRadius: `${borderRadius}px`,
+            borderWidth: `${borderWidth}px`,
+            backgroundImage: `url(${imgData})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            boxShadow: shadow,
+          }}
+        >
+          {children.map((c: IItems) => {
+            const { x, y, w, h, minW, i } = c;
+            return (
+              <div key={i} data-grid={{ x, y, w, h, minW }}>
+                <RenderItem
+                  item={c}
+                  inputValue={inputValue}
+                  setInputValue={setInputValue}
+                  outputValue={outputValue}
+                  setOutputValue={setOutputValue}
+                />
+              </div>
+            );
+          })}
+        </GridLayout>
+      )}
     </section>
   );
 };
