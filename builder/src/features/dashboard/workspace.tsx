460699cf14a525b74125ba56ab1d2d4f9a38ceb9
Aayush <mahapatraaayush@gmail.com>
2022-08-05 11:17:13 +0530

    Fix contract UI issues (#85)
    * Fix contract issues
    
    - Contract components not showing on re-select
    - Spinner was broken
    
    * Fix component naming issue for spinner

--- builder/src/features/dashboard/workspace.tsx
+++ builder/src/features/dashboard/workspace.tsx
@@ -119,8 +119,6 @@ const Workspace: FC<IWorkspace> = ({
 
           let elementArray = newArray;
 
-          console.log(newArray);
-
           setElementConfig({
             ...elementConfig,
             [selector.name]: elementArray,
