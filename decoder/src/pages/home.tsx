873cd2338f737429e2230d9abe4a55389a8649ec
paarth17032000 <paarth17032000@gmail.com>
2022-08-18 14:57:24 +0530

    Fix the width and X coordinate for nft container for any number of cols

--- decoder/src/pages/home.tsx
+++ decoder/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Layout, Layouts, Responsive, WidthProvider } from "react-grid-layout";
 import BuilderConfig from "config";
 import RenderItem from "utils/render-item";
@@ -14,298 +14,239 @@ const Home: FC = () => {
   const [testConfig, setTestConfig] = useState(
     JSON.parse(BuilderConfig).builder
   );
-  // const [prev, setPrev] = useState(null)
+  const [assets, setAssets] = useState([]); // for storing all nfts
+  const [assetNum, setAssetNum] = useState(0); // for rendering nfts one by one
   const [inputVal, setInputVal] = useState('')
-  const [photos , setPhotos] = useState([])
-    console.log(testConfig)
+  const [nftCard, setNftCard] = useState<any>({}); // for rendering nfts one by one
+
+  useEffect(() => {
+    let nftY = null;
+    let nftCols = 0;
+    testConfig.map((item) => {
+      if (item.nft && nftY === null) {
+        nftY = item.y;
+        setNftCard(item);
+      }
+      if (item.nft && item.y === nftY) {
+        nftCols++;
+      }
+    });
+    setTestConfig(testConfig.filter((i) => !i.nft));
+  }, []);
+
+  console.log(testConfig);
 
   // to persist layout changes
   const onLayoutChange = (layout: Layout[], layouts: Layouts) => {
-    console.log(layout)
-    let newItemsArr = layout.map((obj: IItems) => {
-      let selectedItem = testConfig.filter(
-        (item: IItems) => item.i === obj.i
-      )[0];
-      const { h, minW, x, y, w, i, minH } = obj;
-      return (selectedItem = {
-        ...selectedItem,
-        h,
-        minW,
-        minH,
-        x,
-        y,
-        w,
-        i,
-      });
-    });
-
-    newItemsArr.length > 0
-      ? setTestConfig(newItemsArr)
-      : setTestConfig(testConfig);
+    setTestConfig(testConfig);
   };
 
-  const handleClick = async () => {
-    console.log(inputVal)
-
-
-   
+  // render nfts from connected wallet using opensea api
+  const renderTokensForOwner = () => {
     fetch(
-      `https://testnets-api.opensea.io/api/v1/assets?owner=${inputVal}&order_direction=desc&offset=0&limit=20&include_orders=false`,
+      `https://testnets-api.opensea.io/api/v1/assets?owner=0xd6c72729EbCC987b171eCF074993ce3C4e34b9f0&order_direction=desc&offset=0&limit=20&include_orders=false`,
       { method: "GET", headers: { Accept: "application/json" } }
     )
-    .then( res => res.json() )
-    .then( result => {
-      console.log(result)
-      let photosArr = result.assets.map(asset => {
-        // let gridX = 0 , gridY = 0, gridW = 6
-        // console.log(gridX, gridY)
-        let newCI = {
-          i: `${asset.id}`,
-          x: 0,
-          y: 1,
-          h: 3,
-          w: 6,
-          name: "Image",
-          imgData: `${asset.image_url}`,
-          link: "",
-          style: {
-            backgroundColor: { r: "0", g: "0", b: "0" },
-            color: { r: "0", g: "0", b: "0", a: "100" },
-            fontWeight: "normal",
-            fontStyle: "normal",
-            textDecoration: "none",
-            justifyContent: "center",
-            fontSize: 15,
-            deleteComponent: 0,
-            margin: {
-              marginTop: 10,
-              marginRight: 0,
-              marginBottom: 10,
-              marginLeft: 0,
-            },
-            padding: {
-              paddingTop: 0,
-              paddingRight: 0,
-              paddingBottom: 0,
-              paddingLeft: 0,
-            },
-          },
-        };
-        let newCT = {
-          i: `${asset.id}text`,
-          x: 0,
-          y: 1,
-          h: 1,
-          w: 6,
-          name: "Text",
-          value: `${asset.name}`,
-          link: "",
-          style: {
-            backgroundColor: { r: "44", g: "44", b: "44", a: 1 },
-            color: { r: "228", g: "228", b: "228", a: "1" },
-            fontWeight: "normal",
-            fontStyle: "normal",
-            textDecoration: "none",
-            justifyContent: "center",
-            fontSize: 29,
-            deleteComponent: 0,
-            margin: {
-              marginTop: 0,
-              marginRight: 0,
-              marginBottom: 0,
-              marginLeft: 0,
-            },
-            padding: {
-              paddingTop: 0,
-              paddingRight: 0,
-              paddingBottom: 0,
-              paddingLeft: 0,
-            },
-          },
-        }; 
-        // gridY  
-        // return newC
-        let parentComponent = {
-          children: [newCI, newCT],
-          i: asset.id + "container",
-          x: 0,
-          y: 1,
-          h: 6,
-          w: 6,
-          name: "Container",
-          style: {
-            backgroundColor: { r: "44", g: "44", b: "44", a: 1 },
-            borderRadius: 6,
-            borderWidth: 0,
-            color: { r: "0", g: "0", b: "0", a: "100" },
-            deleteComponent: 0,
-            shadow: "none",
-          },
-        };
-        return parentComponent
-      })
-      // let textArr = result.assets.map(text => {
-      //   let gridX = 0 , gridY = 0, gridW = 6
-      //   console.log(gridX, gridY)
-      //   let newCT = {
-      //     i: `${text.id}text`,
-      //     x: 0,
-      //     y: 1,
-      //     h: 3,
-      //     w: 6,
-      //     name: "Text",
-      //     imgData: `${text.name}`,
-      //     link: "",
-      //     style: {
-      //       backgroundColor: { r: "44", g: "44", b: "44", a: 1 },
-      //       color: { r: "228", g: "228", b: "228", a: "1" },
-      //       fontWeight: "normal",
-      //       fontStyle: "normal",
-      //       textDecoration: "none",
-      //       justifyContent: "center",
-      //       fontSize: 29,
-      //       deleteComponent: 0,
-      //       margin: {
-      //         marginTop: 10,
-      //         marginRight: 0,
-      //         marginBottom: 10,
-      //         marginLeft: 0,
-      //       },
-      //       padding: {
-      //         paddingTop: 0,
-      //         paddingRight: 0,
-      //         paddingBottom: 0,
-      //         paddingLeft: 0,
-      //       },
-      //     },
-      //   }; 
-      //   return newC
-      // })
-      
-      let newItemsArr = testConfig.map((item) => {
-        const { y } = item;
-        if (y >= photosArr[0].y) {
+      .then((response) => response.json())
+      .then(({ assets }) => {
+        setAssets(assets);
+        // set no. of cols
+        let cols = 2
+        let colW = 6 / cols 
+        let X = 0
+        let nCardsArr = assets.map((asset: any, index: number) => {
+
+          // check config y if any other elements present above it
+          // let minY = Math.min(testConfig.map((item) => item.y))
+          // let el = testConfig.children?.filter((item) => item.y === minY)[0];
+          // let height = el ? el.h + el.y : 0
+
+          let modifiedX = X
+          X = X+colW 
+          X = X+colW <= 6 ? X :  0
           return {
-            ...item,
-            y: y + photosArr.length*photosArr[0].h,
+            ...nftCard,
+            i: asset.id + "container",
+            x : modifiedX,
+            w: colW,
+            children: [
+              {
+                ...nftCard.children[0],
+                i: asset.id,
+                imgData: asset.image_url,
+              },
+              {
+                ...nftCard.children[1],
+                i: asset.id + "text",
+                value: asset.name,
+              },
+            ],
           };
-        } else {
-          return {
-            ...item,
-            y: y,
-          };
-        }
+        })
+
+
+        let newItemsArr = testConfig.map((item) => {
+          const { y } = item;
+          if (y >= nCardsArr[0].y) {
+            return {
+              ...item,
+              y: y + nCardsArr.length*nCardsArr[0].h,
+            };
+          } else {
+            return {
+              ...item,
+              y: y,
+            };
+          }
+        });
+        // console.log(photos, photosArr)
+        // setTestConfig([...testConfig, ...newItemsArr]);
+        setTestConfig([...newItemsArr, ...nCardsArr]);
+
       });
-      // console.log(photos, photosArr)
-      // setTestConfig([...testConfig, ...newItemsArr]);
-      setTestConfig([...newItemsArr, ...photosArr]);
-    })
-    .catch(err => console.log(err))
-    // const abc = await result.json()
-    // console.log(abc)
-    console.log(testConfig)
-  
-    // let result = await fetchNft(inputVal)
-    // console.log(result)
-    // testConfig.builder
+  };
 
-    // if(photos){
-    //   let photosArr = photos.map(photo => {
-    //     let newC = {
-    //       i: `photo-${photo.albumId}-${photo.id}`,
-    //       x: 0,
-    //       y: 0,
-    //       h: 1,
-    //       w: 6,
-    //       name: "Text",
-    //       value: `${photo.title}`,
-    //       link: "",
-    //       style: {
-    //         backgroundColor: { r: "0", g: "0", b: "0" },
-    //         color: { r: "0", g: "0", b: "0", a: "100" },
-    //         fontWeight: "normal",
-    //         fontStyle: "normal",
-    //         textDecoration: "none",
-    //         justifyContent: "center",
-    //         fontSize: 15,
-    //         deleteComponent: 0,
-    //         margin: {
-    //           marginTop: 0,
-    //           marginRight: 0,
-    //           marginBottom: 0,
-    //           marginLeft: 0,
-    //         },
-    //         padding: {
-    //           paddingTop: 0,
-    //           paddingRight: 0,
-    //           paddingBottom: 0,
-    //           paddingLeft: 0,
-    //         },
-    //       },
-    //     };
-    //     return newC
-    //   })
-    //   setPhotos(photosArr)
-    // }
-    // let newC = {
-    //   i: "testing",
-    //   x: 0,
-    //   y: 0,
-    //   h: 1,
-    //   w: 3,
-    //   name: "Text",
-    //   value: "Testing...",
-    //   link: "",
-    //   style: {
-    //     backgroundColor: { r: "0", g: "0", b: "0" },
-    //     color: { r: "0", g: "0", b: "0", a: "100" },
-    //     fontWeight: "normal",
-    //     fontStyle: "normal",
-    //     textDecoration: "none",
-    //     justifyContent: "center",
-    //     fontSize: 15,
-    //     deleteComponent: 0,
-    //     margin: {
-    //       marginTop: 0,
-    //       marginRight: 0,
-    //       marginBottom: 0,
-    //       marginLeft: 0,
-    //     },
-    //     padding: {
-    //       paddingTop: 0,
-    //       paddingRight: 0,
-    //       paddingBottom: 0,
-    //       paddingLeft: 0,
-    //     },
-    //   },
-    // };
+  // create new nft component
+  const renderImage = (attributes) => {
+    let newC = {
+      i: attributes.id,
+      x: 0,
+      y: 0,
+      h: 6,
+      w: 6,
+      name: "Image",
+      imgData: attributes.image_url,
+      link: "",
+      style: {
+        backgroundColor: { r: "0", g: "0", b: "0" },
+        color: { r: "0", g: "0", b: "0", a: "100" },
+        fontWeight: "normal",
+        fontStyle: "normal",
+        textDecoration: "none",
+        justifyContent: "left",
+        fontSize: 15,
+        deleteComponent: 0,
+        margin: {
+          marginTop: 10,
+          marginRight: 10,
+          marginBottom: 10,
+          marginLeft: 15,
+          fontWeight: "normal",
+        },
+        padding: {
+          paddingTop: 0,
+          paddingRight: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+        },
+      },
+    };
 
-    // let newItemsArr = testConfig.map((item) => {
-    //   // console.log(item)
-    //   const { y } = item;
-    //   return {
-    //     ...item,
-    //     y: y + newC.h,
-    //   };
-    // });
-    // console.log(photos)
+    let textC = {
+      i: attributes.id + "text",
+      x: 0,
+      y: 6,
+      h: 1,
+      w: 6,
+      name: "Text",
+      value: attributes.name,
+      link: "",
+      style: {
+        backgroundColor: { r: "44", g: "44", b: "44", a: 1 },
+        color: { r: "228", g: "228", b: "228", a: "1" },
+        fontWeight: "bold",
+        fontStyle: "normal",
+        textDecoration: "none",
+        justifyContent: "left",
+        fontSize: 20,
+        deleteComponent: 0,
+        margin: {
+          marginTop: 0,
+          marginRight: 0,
+          marginBottom: 0,
+          marginLeft: 15,
+        },
+        padding: {
+          paddingTop: 0,
+          paddingRight: 0,
+          paddingBottom: 0,
+          paddingLeft: 10,
+        },
+      },
+    };
 
-    // let newItemsArr = testConfig.map((item) => {
-    //   const { y } = item;
-    //   if (y >= newC.y) {
-    //     return {
-    //       ...item,
-    //       y: y + newC.h,
-    //     };
-    //   } else {
-    //     return {
-    //       ...item,
-    //       y: y,
-    //     };
-    //   }
-    // });
+    let parentComponent = {
+      children: [newC, textC],
+      i: attributes.id + "container",
+      x: 0,
+      y: 1,
+      h: 7,
+      w: 2,
+      name: "Container",
+      style: {
+        backgroundColor: { r: "44", g: "44", b: "44", a: 1 },
+        borderRadius: 6,
+        borderWidth: 0,
+        color: { r: "0", g: "0", b: "0", a: "100" },
+        deleteComponent: 0,
+        shadow: "none",
+      },
+    };
 
-    setTestConfig([...testConfig, ...photos]);
+    let newItemsArr = testConfig.map((item) => {
+      const { y } = item;
+      if (y >= parentComponent.y) {
+        return {
+          ...item,
+          y: y + parentComponent.h,
+        };
+      } else {
+        return {
+          ...item,
+          y: y,
+        };
+      }
+    });
+
+    setTestConfig([...newItemsArr, parentComponent]);
+    setAssetNum(assetNum + 1);
+  };
+
+  const renderNfts = (attributes) => {
+    let nCard: any = {
+      ...nftCard,
+      i: attributes.id + "container",
+      children: [
+        {
+          ...nftCard.children[0],
+          i: attributes.id,
+          imgData: attributes.image_url,
+        },
+        {
+          ...nftCard.children[1],
+          i: attributes.id + "text",
+          value: attributes.name,
+        },
+      ],
+    };
+
+    let newItemsArr = testConfig.map((item) => {
+      const { y } = item;
+      if (y >= nCard.y) {
+        return {
+          ...item,
+          y: y + nCard.h,
+        };
+      } else {
+        return {
+          ...item,
+          y: y,
+        };
+      }
+    });
+
+    setTestConfig([...newItemsArr, nCard]);
+    setAssetNum(assetNum + 1);
   };
 
   return (
@@ -343,14 +284,26 @@ const Home: FC = () => {
         })}
       </ResponsiveGridLayout>
       <div>
-        <input type='text' value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
+        <input type='text' className="border px-2 py-1 ml-2" value={inputVal} onChange={(e) => setInputVal(e.target.value)}/>
         <button
           className="m-4 px-4 py-2 bg-purple-600 text-white rounded-full"
-          onClick={handleClick}
+          onClick={renderTokensForOwner}
         >
           Fetch
         </button>
       </div>
+      {/* <button
+        className="m-4 px-4 py-2 bg-purple-600 rounded-full shadow-md border border-purple-400"
+        onClick={() => renderNfts(assets[assetNum])}
+      >
+        + Add
+      </button>
+      <button
+        className="m-4 px-4 py-2 bg-green-600 rounded-full shadow-md border border-green-500"
+        onClick={renderTokensForOwner}
+      >
+        Connect Wallet
+      </button> */}
     </main>
   );
 };
