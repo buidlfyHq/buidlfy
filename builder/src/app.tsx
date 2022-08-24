54bf5c5814669792eefcd8818540368d206364dc
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-08-24 14:37:13 +0530

    Merge feat/new-design with dev resolving conflicts

--- builder/src/app.tsx
+++ builder/src/app.tsx
@@ -3,10 +3,6 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "pages/home";
 import Dashboard from "pages/dashboard";
 import Template from "pages/templates";
-<<<<<<< HEAD
-import { Web3Storage } from "web3.storage";
-=======
->>>>>>> b50f8f7310d58d824de28a99e1816a021e67eb8c
 
 const App: FC = () => {
   useEffect(() => {
@@ -17,11 +13,6 @@ const App: FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
-        {/* <Route path="/templates" element={<Template />} /> */}
-=======
-        <Route path="/templates" element={<Template />} />
->>>>>>> b50f8f7310d58d824de28a99e1816a021e67eb8c
       </Routes>
     </Router>
   );
