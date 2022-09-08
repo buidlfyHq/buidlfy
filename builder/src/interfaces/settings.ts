756f58a83739cc826f812af4ec111356ac925fde
aayushmahapatra <dr.aaaaaaaa8@gmail.com>
2022-09-08 12:32:28 +0530

    Update refactor for initializing settings refactor

--- builder/src/interfaces/settings.ts
+++ builder/src/interfaces/settings.ts
@@ -1,44 +1,16 @@
-import { ChangeEvent } from "react";
-import IColor from "./color";
 import IItems from "./items";
 
 export default interface ISettings {
-  items?: IItems[];
-  setItems?: (items: IItems[]) => void;
   selectedItem?: IItems;
-  setLink?: (link: string) => void;
-  link?: string;
-  setValue?: (value: string) => void;
-  value?: string;
-  setBold?: (bold: string | boolean) => void;
-  bold?: string;
-  setItalic?: (italic: string | boolean) => void;
-  italic?: string;
-  setUnderline?: (underline: string | boolean) => void;
-  underline?: string;
-  setColor?: (color: IColor) => void;
-  color?: IColor;
-  setBgColor?: (backgroundColor: IColor) => void;
-  backgroundColor?: IColor;
-  setDeleteComponent?: (deleteComponent: number) => void;
-  deleteComponent?: number;
-  justifyContent?: string;
-  setLeft?: (justifyContent: string | boolean) => void;
-  setCenter?: (justifyContent: string | boolean) => void;
-  setRight?: (justifyContent: string | boolean) => void;
-  setFontSize?: (fontSize: number) => void;
-  fontSize?: number;
-  setContractConfig?: (contractConfig: {
-    abi: string;
-    address: string;
+  setShowComponent?: (showComponent: {
+    id: string;
+    value: {
+      name: string;
+      inputs: object[];
+      outputs: object[];
+      stateMutability: string;
+    };
   }) => void;
-  contractConfig?: { abi: string; address: string };
-  setShowComponent?: (showComponent: { id: string; value: {
-    name: string;
-    inputs: object[];
-    outputs: object[];
-    stateMutability: string;
-  }; }) => void;
   showComponent?: {
     id: string;
     value: {
@@ -48,59 +20,16 @@ export default interface ISettings {
       stateMutability: string;
     };
   };
-  selector?: {
-    methodName: string;
-    type: string;
-    name: string;
-    buttonId: string;
-  },
-  setSelector?: (selector: {
-    methodName: string;
-    type: string;
-    name: string;
-    buttonId: string;
-  }) => void;
   setOpenSetting?: (open: boolean) => void;
   settingItemId?: string;
   elementConfig?: object;
   openTab?: number;
   setOpenTab?: (openTab: number) => void;
-  borderRadius?: number;
-  setBorderRadius?: (borderRadius: number) => void;
-  borderWidth?: number;
-  setBorderWidth?: (borderWidth: number) => void;
-  margin?: {
-    marginLeft?: number,
-    marginRight?: number,
-    marginTop?: number,
-    marginBottom?: number,
-  };
-  padding?: {
-    paddingLeft?: number,
-    paddingRight?: number,
-    paddingTop?: number,
-    paddingBottom?: number,
-  }
-  setMarginLeft?: (marginLeft: number) => void;
-  setMarginRight?: (marginRight: number) => void;
-  setMarginTop?: (marginTop: number) => void;
-  setMarginBottom?: (marginBottom: number) => void;
-  setPaddingLeft?: (paddingLeft: number) => void;
-  setPaddingRight?: (paddingRight: number) => void;
-  setPaddingTop?: (paddingTop: number) => void;
-  setPaddingBottom?: (paddingBottom: number) => void;
-  setSmall?: (shadow: string | boolean) => void;
-  setMedium?: (shadow: string | boolean) => void;
-  setLarge?: (shadow: string | boolean) => void;
-  shadow?: string;
-  setOn?: (connectWallet: string | boolean) => void;
-  connectWallet?: string;
-  placeholder?: string;
-  setPlaceholder?: (placeholder: string) => void;
   handlePlaceholderChange?: (e) => void;
-  textVal?: string;
-  // handleTextChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleTextChange?: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
-  linkVal?: string;
+  handleTextChange?: (
+    e:
+      | React.ChangeEvent<HTMLTextAreaElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => void;
   handleLinkChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
