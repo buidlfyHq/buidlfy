bbbdf4420c5248bea71b1258dad26d8124953f48
aayushmahapatra <dr.aaaaaaaa8@gmail.com>
2022-09-16 19:34:54 +0530

    Move handleSave to redux

--- builder/src/redux/workspace/workspace.reducers.ts
+++ builder/src/redux/workspace/workspace.reducers.ts
@@ -51,7 +51,7 @@ const workspaceSlice = createSlice({
       );
       return { ...state, workspaceElements: updatedElements };
     },
-    
+
     // to update the elements
     updateWorkspaceElementsArray(
       state,
@@ -69,6 +69,83 @@ const workspaceSlice = createSlice({
           action.payload
         ),
       };
+    },
+
+    saveContractConfig(state: IWorkspaceState, action: { payload }) {
+      const {contractElementSelected} = action.payload;
+      // filter last selected element
+      const filteredObject = contractElementSelected[
+        currentElement.name
+      ]?.filter(
+        (key: { buttonId: string }) => key.buttonId === selectedItem.i
+      )[0];
+
+      let updatedContract = {};
+
+      let duplicate = selectedItem.contract.inputs?.find(
+        (e: { id: string }) => e.id === filteredObject.id
+      );
+
+      if (!duplicate) {
+        if (currentElement.type === "input") {
+          updatedContract = {
+            ...selectedItem.contract,
+            inputs: [
+              ...selectedItem.contract.inputs,
+              { id: filteredObject.id, send: false },
+            ],
+          };
+        } else if (currentElement.type === "send") {
+          updatedContract = {
+            ...selectedItem.contract,
+            inputs: [
+              ...selectedItem.contract.inputs,
+              { id: filteredObject.id, send: true },
+            ],
+          };
+        } else if (currentElement.type === "output") {
+          updatedContract = {
+            ...selectedItem.contract,
+            outputs: [
+              ...selectedItem.contract.outputs,
+              { id: filteredObject.id },
+            ],
+          };
+        }
+      } else {
+        updatedContract = { ...selectedItem.contract };
+      }
+
+      let updatedItem = {
+        ...selectedItem,
+        contract: updatedContract,
+      };
+
+      // search id in items
+      const elementsIndex = workspaceElements.findIndex(
+        (item) => item.i === selectedItem.i
+      );
+
+      if (elementsIndex === -1) {
+        // search id in children
+        const updatedItems = workspaceElements.map((item) => {
+          const childIndex = item.children?.findIndex(
+            (child: IWorkspaceElement) => child.i === selectedItem.i
+          );
+          let newArray = [...item?.children];
+          newArray[childIndex] = updatedItem;
+          return {
+            ...item,
+            children: newArray,
+          };
+        });
+
+        dispatch(updateWorkspaceElementsArray(updatedItems));
+      } else {
+        let newArray = [...workspaceElements];
+        newArray[elementsIndex] = updatedItem;
+        dispatch(updateWorkspaceElementsArray(newArray));
+      }
     },
   },
 });
