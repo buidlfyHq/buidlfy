ff1345dec425e869272f8ecc08791bd6a8992110
Aayush <mahapatraaayush@gmail.com>
2022-09-18 20:17:52 +0530

    Remove login page (#122)

--- builder/src/app.tsx
+++ builder/src/app.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "pages/home";
 import Dashboard from "pages/dashboard";
 
 const App: FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
