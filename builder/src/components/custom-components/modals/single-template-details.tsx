cba24c7f46abaf1c2c7d83119badfac1764c1bd2
paarth17032000 <paarth17032000@gmail.com>
2022-09-29 00:03:29 +0530

    Add icons and images and minor fixes in UI of template and single-template modals

--- builder/src/components/custom-components/modals/single-template-details.tsx
+++ builder/src/components/custom-components/modals/single-template-details.tsx
@@ -3,6 +3,9 @@ import { Dialog } from '@headlessui/react'
 import {IoArrowBack} from 'react-icons/io5'
 import {VscArrowRight} from 'react-icons/vsc'
 import SignleTempImg from 'assets/modalIcons/single-temp-img.png'
+import EyeImg from 'assets/eye.png'
+import TempexImg from 'assets/tempex.png'
+import InfoCircleImg from 'assets/info-circle.png'
 
 interface MintedTemplateModal {
     isOpenSingleTemplate: boolean;
@@ -17,14 +20,56 @@ const SingleTemplateDetails : FC<MintedTemplateModal>  = ({
 }) => {
     
     const handleSubmit = () => {
+        setIsOpenSingleTemplate(false)
         setIsOpenFinalTemplate(true)
     }
 
     return (
         <Dialog className="relative z-50" open={isOpenSingleTemplate} onClose={() => setIsOpenSingleTemplate(false)}>
             <div className="fixed inset-0 flex items-center justify-center bg-black/70 backdrop-blur-[10px]">
-            <Dialog.Panel className="grid grid-cols-12 gap-12 w-full max-w-[1200px] my-20 mx-28 rounded-[24px] py-12 px-10 bg-white max-h-[80vh]">
-                <div className="flex flex-col justify-start col-span-4">
+            <Dialog.Panel className="w-full max-w-[1200px] my-20 mx-28 rounded-[24px] py-10 px-14 bg-white">
+                <div className="flex items-center justify-between">
+                    <div className="text-[22px] font-[500] text-[#14142B]">Cryptin Next Generation Web Template</div>
+                    <div className="preview-button flex items-center py-2.5 px-6 rounded-[24px]">
+                        <img src={EyeImg} alt="icon" width={18} height={18} />
+                        <div className="ml-2">Preview</div>
+                    </div>
+                </div>
+                <div className="mt-5">
+                    <img src={TempexImg} className="w-full max-h-[509px] h-auto rounded-[28px]" alt="icon"  height={669} />
+                    <div className="flex justify-between">
+                        <div className="mt-5">
+                            <div className="flex items-center mt-3">
+                                <div className="bg-[#9CB0D7] w-[32px] h-[32px] rounded-[50%] mt-30">{" "}</div>
+                                <div className="ml-2 text-[14px] text-[#14142B] opacity-70">Stevan Mark - 0xBBB6...e96e</div>
+                            </div>
+                            <div className="mt-8 text-[#4E4B66] opacity-70 text-[13px] max-w-[400px]">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum felis, sed ullamcorper tempus faucibus in imperdiet semper justo mauris sed.    
+                            </div>
+                        </div>
+                        <div>
+                            <div className="w-full mt-8 text-center text-[14px] text-[#202525] bg-[#E6EAF4] rounded-[4px] font-[600] py-4">
+                                $149.00
+                            </div>
+                            <div
+                                onClick={handleSubmit}
+                                className="w-full flex justify-center items-center mt-5 text-center text-[22px] text-white cursor-pointer rounded-[8px] font-[500] py-4 connect-wallet-button"
+                            >
+                                <div className="text-[14px]">Connect Wallet to buy </div>
+                                <VscArrowRight className="ml-2 text-[18px]" />
+                            </div>
+                            <div className="flex mt-3 bg-gray-100 rounded-[4px] items-center text-[#4E4B66] opacity-70 text-[13px] py-3 px-4">
+                                <img src={InfoCircleImg} alt="icon" width={17} height={17} />
+                                <div className="ml-2">To buy this you must connect your wallet</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-7 flex gap-3">
+                    <div className="w-auto py-2 px-3.5 bg-gray-100 text-[#5799EB] rounded-[3px] text-[14px] font-[500]">Web3</div>
+                    <div className="w-auto py-2 px-3.5 bg-gray-100 text-[#5799EB] rounded-[3px] text-[14px] font-[500]">Crypto</div>
+                </div>
+                {/* <div className="flex flex-col justify-start col-span-4">
                     <div className="flex items-center justify-start w-full mt-8 cursor-pointer">
                         <IoArrowBack className="text-[16px] text-[#4E4B66]" /> 
                         <div className="ml-2 text-[13px] font-[500] text-[#4E4B66]">Go Back</div>
@@ -61,7 +106,7 @@ const SingleTemplateDetails : FC<MintedTemplateModal>  = ({
                 </div>
                 <div className="flex justify-center col-span-8">
                     <img src={SignleTempImg} alt="img_temp" width={735} height={763} />
-                </div>
+                </div> */}
             </Dialog.Panel>
             </div>
         </Dialog>
