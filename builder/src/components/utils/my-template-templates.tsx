e61b524b220ae1122235d94fc9a6e2661fab79c7
paarth17032000 <paarth17032000@gmail.com>
2022-10-11 11:15:28 +0530

    mapped menuItems in my-templates

--- builder/src/components/utils/my-template-templates.tsx
+++ builder/src/components/utils/my-template-templates.tsx
@@ -1,11 +1,12 @@
-import TemplateModal from 'features/dashboard/template-modal'
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { toggleModal, toggleModalType } from 'redux/modal/modal.reducers';
-import Temp1 from "assets/icons/temp-1.png";
 import { Menu } from '@headlessui/react';
-const templates = [Temp1,Temp1,Temp1,Temp1]
+import TemplateModal from 'features/dashboard/template-modal'
 import { ReactComponent as SettingsIcon } from "assets/svgAsIcons/dots.svg";
+import Temp1 from "assets/icons/temp-1.png";
+const templates = [Temp1,Temp1,Temp1,Temp1]
+const menu = [{name: 'Edit site'},{name: 'View site'}]
 
 export default function MyTemplateTemplates() {
 const dispatch = useDispatch()  
@@ -23,33 +24,24 @@ const dispatch = useDispatch()
                                 <div className='flex justify-end mr-2 py-1 px-3 text-[#14142B] text-[10px] bg-[#FFE6B0] rounded-[5px]'>
                                     In Review
                                 </div>
-                                {/* <div className=''> */}
-                                    <Menu>
-                                        <Menu.Button className="p-1 bg-white rounded-[50%]">
-                                            <SettingsIcon />
-                                        </Menu.Button>
-                                        <Menu.Items className="absolute right-0 bg-white shadow-template-box flex flex-col rounded-[4px] bg-white px-3 py-2 top-8 z-[10]">
+                                <Menu>
+                                    <Menu.Button className="p-1 bg-white rounded-[50%]">
+                                        <SettingsIcon />
+                                    </Menu.Button>
+                                    <Menu.Items className="absolute right-0 bg-white shadow-template-box flex flex-col rounded-[4px] bg-white px-3 py-2 top-8 z-[10]">
+                                        {menu.map(menu => (
                                             <Menu.Item>
                                                 {({ active }) => (
                                                 <a
                                                     className={`whitespace-nowrap text-[13px] px-2 py-1 text-[#14142B] ${active && ' bg-gray-200 rounded-[4px]'}`}
                                                 >
-                                                    Edit site
+                                                    {menu.name}
                                                 </a>
                                                 )}
                                             </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                <a
-                                                    className={`whitespace-nowrap text-[13px] text-[#14142B] px-2 py-1 ${active && 'bg-gray-200 rounded-[4px]'}`}
-                                                >
-                                                    View site
-                                                </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Menu>
-                                {/* </div> */}
+                                        ))}
+                                    </Menu.Items>
+                                </Menu>
                             </div>
                             <div className='absolute flex flex-col items-center justify-center w-full h-full font-[13px] font-[600]'>
                                 <div className='py-2 px-10 rounded-[8px] bg-white text-[#7743E7]'>View Details</div>
