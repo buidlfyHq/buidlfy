75720ab6552134c55177862c4fa8c09acafa55b1
Mitrasish Mukherjee <34746546+rekpero@users.noreply.github.com>
2022-10-19 18:46:38 +0530

    Created Deployment API for interacting with Spheron API (#164)
    * Add server app for deployment trigger
    
    * Add socket io server side
    
    * Fix socket server creation and connection establishment
    
    * Add logic for socket emit when deployment is in progress
    
    * Add domain update logic for deployment
    
    * Update domain attachment and verification, Update decoder to take build env var for decoding
    
    * Add loggers for errors and info and fix some nomenclature
    
    * Update route name of endpoints in deployment
    
    * Add update subdomain endpoint to update a new deployment if domain is already present
    
    * Update spheron deployment to not push any comment when deployment

---
+++ server/src/config/index.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const {
+  NODE_ENV,
+  PORT,
+  SECRET_KEY,
+  LOG_FORMAT,
+  LOG_DIR,
+  ORIGIN,
+  SPHERON_API_HOST,
+  DEPLOYMENT_ENDPOINT,
+  SPHERON_TOKEN,
+  ORGANIZATION_ID,
+  DECODER_GIT_URL,
+  CORS_ADDRESS,
+  SITE_DOMAIN_NAME,
+  CLOUDFLARE_KEY,
+  CLOUDFLARE_EMAIL,
+  CDN_CNAME,
+  CLOUDFLARE_ZONE_ID,
+} = process.env;
---
+++ server/src/routes/deployments.route.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import DeploymentsController from '@/controllers/deployments.controller';
+import { Routes } from '@interfaces/routes.interface';
+import validationMiddleware from '@/middlewares/validation.middleware';
+import { DeployAppDto, CreateDeploymentSubdomainDto, VerifyDeploymentSubdomainDto, UpdateDeploymentSubdomainDto } from '@/dtos/deployments.dto';
+
+class DeploymentsRoute implements Routes {
+  public path = '/deployment';
+  public router = Router();
+  public deploymentController = new DeploymentsController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}/create`, validationMiddleware(DeployAppDto, 'body'), this.deploymentController.startDeployment);
+    this.router.put(
+      `${this.path}/create-subdomain`,
+      validationMiddleware(CreateDeploymentSubdomainDto, 'body'),
+      this.deploymentController.createDeploymentSubdomain,
+    );
+    this.router.patch(
+      `${this.path}/update-subdomain`,
+      validationMiddleware(UpdateDeploymentSubdomainDto, 'body'),
+      this.deploymentController.updateDeploymentSubdomain,
+    );
+    this.router.patch(
+      `${this.path}/verify-subdomain`,
+      validationMiddleware(VerifyDeploymentSubdomainDto, 'body'),
+      this.deploymentController.verifyDeploymentSubdomain,
+    );
+  }
+}
+
+export default DeploymentsRoute;
