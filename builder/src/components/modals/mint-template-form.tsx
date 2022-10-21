7817584a0d85eeb442889b09cf54dd4e702f2b1b
aayushmahapatra <dr.aaaaaaaa8@gmail.com>
2022-10-21 12:02:54 +0530

    Remove log

--- builder/src/components/modals/mint-template-form.tsx
+++ builder/src/components/modals/mint-template-form.tsx
@@ -49,7 +49,6 @@ const MintTemplateForm: FC = () => {
       value: workspaceElements,
     };
 
-    console.log("NewTemplate ->", newTemplate);
     console.log("JSON.stringify(newTemplate): ", JSON.stringify(newTemplate));
     const templateCID = await uploadTemplateToWeb3Storage(
       JSON.stringify(newTemplate)
