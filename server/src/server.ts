b60c995c4e3099620809b454536289cd64e512db
dusanstanis <58050163+dusanstanis@users.noreply.github.com>
2022-10-31 16:38:37 +0100

    Change to docker file and tsconfig & add healthcheck router (#200)
    * Change to docker file and tsconfig. Add healthcheck router
    
    * Change uuid creation from crypto dep to uuid dep
    
    Co-authored-by: Mitrasish Mukherjee <mmitrasish97@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import App from '@/app';
 import DeploymentsRoute from '@routes/deployments.route';
 import validateEnv from '@utils/validateEnv';
+import HealthRoute from './routes/health.route';
 import UploadRoute from './routes/upload.route';
 
 validateEnv();
 
-const app = new App([new DeploymentsRoute(), new UploadRoute()]);
+const app = new App([new DeploymentsRoute(), new UploadRoute(), new HealthRoute()]);
 
 app.listen();
