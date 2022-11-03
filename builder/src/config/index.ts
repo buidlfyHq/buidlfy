96690915a4b7f4ab532c286f5f508de0349df6db
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-11-03 21:57:01 +0530

    Remove unused code

--- builder/src/config/index.ts
+++ builder/src/config/index.ts
@@ -113,7 +113,7 @@ const local: IConfig = {
       "https://api.thegraph.com/subgraphs/name/buidlfyhq/buidlfy-template-testnet",
   },
   server: {
-    SERVER: "http://localhost:8080/",
+    SERVER: "https://api.buidlfy.com/",
   },
   network: {
     DEFAULT_NETWORK: {
