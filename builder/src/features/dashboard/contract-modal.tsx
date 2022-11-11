923a048642f78e12317254a6fa254b83a376ef1b
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2022-11-12 04:34:17 +0530

    Add seperate file for network names

--- builder/src/features/dashboard/contract-modal.tsx
+++ builder/src/features/dashboard/contract-modal.tsx
@@ -9,8 +9,8 @@ import {
 } from "redux/contract/contract.reducers";
 import upload from "assets/upload-img.svg";
 import { IContract } from "redux/contract/contract.interfaces";
+import { networkNames } from "config/network";
 import "styles/components.css";
-import { IRootState } from "redux/root-state.interface";
 
 interface IContractModal {
   isOpen: boolean;
@@ -80,36 +80,6 @@ const ContractModal: FC<IContractModal> = ({
     };
   };
 
-  const networkNames = [
-    {
-      id: 1,
-      name: "Ethereum Mainnet",
-    },
-    {
-      id: 137,
-      name: "Polygon Mainnet",
-    },
-    {
-      id: 56,
-      name: "Binance Smart Chain",
-    },
-    {
-      id: 10,
-      name: "Optimism",
-    },
-    {
-      id: 80001,
-      name: "Mumbai Testnet",
-    },
-    {
-      id: 97,
-      name: "BSC Testnet",
-    },
-    {
-      id: 420,
-      name: "Optimism Goerli Testnet",
-    },
-  ];
   return (
     <Dialog
       as="div"
