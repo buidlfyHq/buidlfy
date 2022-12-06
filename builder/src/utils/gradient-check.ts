f1383b5f6ec5f7176b14324971b6b036a46f7898
Aayush Mahapatra <aayushmahapatra@Aayushs-MacBook-Air.local>
2022-12-06 21:06:53 +0530

    Test prettier

--- builder/src/utils/gradient-check.ts
+++ builder/src/utils/gradient-check.ts
@@ -1,8 +1,8 @@
 export const gradientCheck = (color: string, background: boolean) => {
-  const gradientCondition = color?.indexOf("gradient") !== -1;
+  const gradientCondition = color?.indexOf('gradient') !== -1;
   if (background) {
-    return gradientCondition ? color : "transparent";
+    return gradientCondition ? color : 'transparent';
   } else {
-    return gradientCondition ? "transparent" : color;
+    return gradientCondition ? 'transparent' : color;
   }
 };
