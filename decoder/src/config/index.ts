a76de44f48ce2e960d08e88a1816fafab7bd596e
Aayush <mahapatraaayush@gmail.com>
2022-12-19 23:50:19 +0530

    Generate config with unicode characters (#287)

--- decoder/src/config/index.ts
+++ decoder/src/config/index.ts
@@ -3,8 +3,8 @@ import { decode as base64_decode } from "base-64";
 export const OracleContractAddress =
   "0x36dA71ccAd7A67053f0a4d9D5f55b725C9A25A3E";
 
-const BuilderConfig = base64_decode(
-  process.env.REACT_APP_BUIDLFY_CONFIGURATION || ""
+const BuilderConfig = decodeURIComponent(
+  base64_decode(process.env.REACT_APP_BUIDLFY_CONFIGURATION || "")
 );
 
 export default BuilderConfig;
