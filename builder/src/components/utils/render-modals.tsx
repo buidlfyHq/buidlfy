44dbd6e31f2105d31d039726dc4ca06df18d3926
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2023-02-02 13:31:31 +0530

    Add spheron site link

--- builder/src/components/utils/render-modals.tsx
+++ builder/src/components/utils/render-modals.tsx
@@ -24,7 +24,7 @@ const RenderModal: FC = () => {
 
   switch (modalType) {
     case 'start':
-      return <PublishProgress />;
+      return <StartModal />;
     case 'template':
       return <SelectTemplateModal />;
     case 'single':
